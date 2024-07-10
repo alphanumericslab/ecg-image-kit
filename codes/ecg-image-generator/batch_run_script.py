9d9d1a94cbf4ea7a9218de8b6d4f7fef45e8f537
Deepanshi <deepanshi.asr.21@gmail.com>
2024-07-10 15:23:16 -0400

    ecg gen: batch gen pickle code and multiprocess running script

---
+++ codes/ecg-image-generator/batch_run_script.py
@@ -0,0 +1,39 @@
+import os
+import wfdb
+from PIL import Image
+import json
+import matplotlib.pyplot as plt
+import subprocess
+import pickle
+import multiprocessing
+import time
+
+f1 = os.path.join(os.getcwd(), "../../../output/new1/textfile.pkl")
+
+with open(f1, 'rb') as f:
+    loaded_list = pickle.load(f)
+
+st_time = time.time()
+for line in loaded_list[:10]:
+    print(line)
+    process = subprocess.Popen(line, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    stdout, stderr = process.communicate()
+    
+    if process.returncode == 0:
+        print("Script executed successfully")
+        print("Output:", stdout)
+    else:
+        print("Script execution failed")
+        print("Error:", stderr)
+
+en_time = time.time()
+print(en_time - st_time)
+
+def run_subprocess(cmd):
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+st_time = time.time()
+with multiprocessing.Pool() as pool:
+        pool.map(run_subprocess, loaded_list[:10])
+
+print(time.time() - st_time)
--- codes/ecg-image-generator/gen_images_batch.py
+++ codes/ecg-image-generator/gen_images_batch.py
@@ -7,6 +7,7 @@ from scipy.stats import bernoulli
 from helper_functions import find_records
 import warnings
 import wfdb
+import pickle
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 warnings.filterwarnings("ignore")
@@ -16,63 +17,20 @@ def get_parser():
     parser.add_argument('-i', '--input_directory', type=str, required=True)
     parser.add_argument('-o', '--output_directory', type=str, required=True)
     parser.add_argument('-se', '--seed', type=int, required=False, default = -1)
-    parser.add_argument('--num_leads',type=str,default='twelve')
-    parser.add_argument('--num_images',type=int,default = -1)
-    
-    #parser.add_argument('-r','--resolution',type=int,required=False,default = 200)
-    #parser.add_argument('--pad_inches',type=int,required=False,default=0)
-    #parser.add_argument('-ph','--print_header', action="store_true",default=False)
-    #parser.add_argument('--full_mode', type=str,default='II')
-
-    #parser.add_argument('--random_grid_color',action="store_true",default=False)
-    #parser.add_argument('--standard_grid_color', type=int, default=5)
-    #parser.add_argument('--random_bw',type=float,default=0)
-
     parser.add_argument('-l', '--link', type=str, required=False,default='')
-    #parser.add_argument('-n','--num_words',type=int,required=False,default=5)
-    #parser.add_argument('--x_offset',dest='x_offset',type=int,default = 30)
-    #parser.add_argument('--y_offset',dest='y_offset',type=int,default = 30)
-    #parser.add_argument('--hws',dest='handwriting_size_factor',type=float,default = 0.2)
-    
-    #parser.add_argument('-ca','--crease_angle',type=int,default=90)
-    #parser.add_argument('-nv','--num_creases_vertically',type=int,default=10)
-    #parser.add_argument('-nh','--num_creases_horizontally',type=int,default=10)
-
-    #parser.add_argument('-rot','--rotate',type=int,default=0)
-    #parser.add_argument('-noise','--noise',type=int,default=50)
-    #parser.add_argument('-c','--crop',type=float,default=0.01)
-
-    #parser.add_argument('--deterministic_offset',action="store_true",default=False)
-    #parser.add_argument('--deterministic_num_words',action="store_true",default=False)
-    #parser.add_argument('--deterministic_hw_size',action="store_true",default=False)
-
-    #parser.add_argument('--deterministic_angle',action="store_true",default=False)
-    #parser.add_argument('--deterministic_vertical',action="store_true",default=False)
-    #parser.add_argument('--deterministic_horizontal',action="store_true",default=False)
-
-    #parser.add_argument('--deterministic_rot',action="store_true",default=False)
-    #parser.add_argument('--deterministic_noise',action="store_true",default=False)
-    #parser.add_argument('--deterministic_crop',action="store_true",default=False)
-    #parser.add_argument('--deterministic_temp',action="store_true",default=False)
-
-    #parser.add_argument('--fully_random',action='store_true',default=False)
-    #parser.add_argument('--hw_text',action='store_true',default=False)
-    #parser.add_argument('--wrinkles',action='store_true',default=False)
-    #parser.add_argument('--augment',action='store_true',default=False)
-    #parser.add_argument('--bbox',action='store_true',default=False)
 
     return parser
 
 def run(args):
         P1 = ['150', '300', '200', '250']  # E.g., resolutions
-        P2 = ['0', '0.1', '0.5']  # E.g., padding inches
+        P2 = ['0', '1', '2']  # E.g., padding inches
         P3 = ['0', '1']  # E.g., random DC offset options
         P4 = [1, 3, 2, 5, 4] # Grid color
         P5 = [1, 0] #add header
         P6 = [3, 4, 5] #number of handwritten words
-        P7 = [30, 50, 100, 20, 70] #x offset for hw
-        P8 = [50, 30, 70] # y offser for hw
-        P9 = [0.1 , 0.3, 0.2] #handwritten size factor
+        P7 = [30, 50, 100, 70, 80] #x offset for hw
+        P8 = [50, 80, 90, 40] # y offser for hw
+        P9 = [0.4 , 0.3, 0.2] #handwritten size factor
         P10 = [90, 60, 20, 50, 40] #crease angle
         P11 = [5, 7, 9, 8, 10] #num creases horizontally
         P12 = [4, 9, 8, 10, 5] #num creases vertically
@@ -82,7 +40,7 @@ def run(args):
         P16 = [1, 0] #hw  text
         P17 = [1, 0] #wrinkles
         P18 = [0, 1] #augment 
-        P19  = [1, 0] #random black and white
+        P19  = [1, 0, 1, 1, 0] #random black and white
         P20 =   [2, 4, 3] #num_columns
         P21 = [1, 2, 3, 4] #long leads
 
@@ -111,6 +69,7 @@ def run(args):
 
         random.seed(args.seed)
 
+        output_dir = args.output_directory
         if os.path.isabs(args.input_directory) == False:
             args.input_directory = os.path.normpath(os.path.join(os.getcwd(), args.input_directory))
         if os.path.isabs(args.output_directory) == False:
@@ -127,75 +86,105 @@ def run(args):
         i = 0
         full_header_files, full_recording_files = find_records(args.input_directory, original_output_dir)
         
-        with open(os.path.join(args.output_directory, 'textfile.txt'), 'w') as f:
-            for full_header_file, full_recording_file in zip(full_header_files, full_recording_files):
-                filename = full_recording_file
-                header = full_header_file
-                args.input_file = os.path.join(args.input_directory, filename)
-                args.header_file = os.path.join(args.input_directory, header)
-                args.start_index = -1
-                _, fields = wfdb.rdsamp(os.path.splitext(args.header_file)[0])
-                currentLeads = random.sample(fields['sig_name'], next(iter_P21))
+        all_commands = []
+        
+        for full_header_file, full_recording_file in zip(full_header_files, full_recording_files):
+            filename = full_recording_file
+            header = full_header_file
+            args.input_file = os.path.join(args.input_directory, filename)
+            args.header_file = os.path.join(args.input_directory, header)
+            args.start_index = -1
+            _, fields = wfdb.rdsamp(os.path.splitext(args.header_file)[0])
+            currentLeads = random.sample(fields['sig_name'], next(iter_P21))
 
-                folder_struct_list = full_header_file.split('/')[:-1]
-                args.output_directory = os.path.join(original_output_dir, '/'.join(folder_struct_list))
-    
-                random_add_header = next(iter_P5)          
+            folder_struct_list = full_header_file.split('/')[:-1]
+            args.output_directory = os.path.join(original_output_dir, '/'.join(folder_struct_list))
+
+            random_add_header = next(iter_P5)          
+            
+            hw_text = next(iter_P16)
+            wrinkles = next(iter_P17)
+            augment = next(iter_P18)
+
+            #Handwritten text addition
+            if(hw_text):
+                num_words = next(iter_P6)
+                x_offset = next(iter_P7)
+                y_offset = next(iter_P8)
+
+            if(wrinkles):
+                crease_angle = next(iter_P10)
+                num_creases_vertically = next(iter_P11)
+                num_creases_horizontally = next(iter_P12)
+
+            
+            if(augment):
+                noise = next(iter_P14) 
+                crop = next(iter_P15)
+                blue_temp = random.choice((True,False))
+
+                if(blue_temp):
+                    temp = random.choice(range(2000,4000))
+                else:
+                    temp = random.choice(range(10000,20000))
+
+            args_dict = dict()
+            config_args = []
+            args_dict["input_file"] = args.input_file
+            args_dict["header_file"] = args.header_file
+            args_dict["output_directory"] = args.output_directory
+            args_dict["start_index"] = args.start_index
+            args_dict["resolution"] = next(iter_P1)
+            args_dict["pad_inches"] = next(iter_P2)
+            args_dict["standard_grid_color"] = next(iter_P4)
+            args_dict["store_config"] = 1
+            args_dict["calibration_pulse"] = next(iter_P3)
+            args_dict["random_bw"] = next(iter_P19)
+            args_dict["num_columns"] = next(iter_P20)
+            args_dict["full_mode"] = currentLeads
+
+            if random_add_header == 1:
+                config_args.append("print_header")
+
+            config_args.append("lead_bbox")
+            config_args.append("lead_name_bbox")
+            
+            if hw_text:
+                config_args.append("hw_text")
+                config_args.append("deterministic_num_words")
+                config_args.append("deterministic_offset")
+                args_dict["num_words"] = num_words
+                args_dict["x_offset"] = x_offset
+                args_dict["y_offset"] = y_offset
+                args_dict["link"] = args.link
+                args_dict["hws"] = next(iter_P9)
+
+            if wrinkles:
+                config_args.append("wrinkles")
+                config_args.append("deterministic_angle")
+                config_args.append("deterministic_vertical")
+                config_args.append("deterministic_horizontal")
+                args_dict["crease_angle"] = crease_angle
+                args_dict["num_creases_vertically"] = num_creases_vertically
+                args_dict["num_creases_horizontally"] = num_creases_horizontally
                 
-                hw_text = next(iter_P16)
-                wrinkles = next(iter_P17)
-                augment = next(iter_P18)
+            if augment:
+                config_args.append("augment")
+                config_args.append("deterministic_noise")
+                args_dict["noise"] = noise
+                args_dict["rotate"] = next(iter_P13)
+                args_dict["crop"] = crop
 
-                #Handwritten text addition
-                if(hw_text):
-                    num_words = next(iter_P6)
-                    x_offset = next(iter_P7)
-                    y_offset = next(iter_P8)
+            command = ["python", "gen_ecg_image_from_data.py"]
+            for key, value in args_dict.items():
+                command.extend([f"--{key}", str(value)])
 
-                if(wrinkles):
-                    crease_angle = next(iter_P10)
-                    num_creases_vertically = next(iter_P11)
-                    num_creases_horizontally = next(iter_P12)
+            for key in config_args:
+                command.extend([f"--{key}"])
+            all_commands.append(command)
 
-                
-                if(augment):
-                    noise = next(iter_P14) 
-                    crop = next(iter_P15)
-                    blue_temp = random.choice((True,False))
-
-                    if(blue_temp):
-                        temp = random.choice(range(2000,4000))
-                    else:
-                        temp = random.choice(range(10000,20000))
-
-                command = "python gen_ecg_image_from_data.py --input_file " + args.input_file + " --header_file " + args.header_file + " --output_directory " + args.output_directory + " --start_index " + str(args.start_index)
-                command += " --resolution " + str(next(iter_P1)) + " --pad_inches " + str(next(iter_P2)) + " --random_add_header " + str(random_add_header) + " --standard_grid_color " + str(next(iter_P4))
-                command += " --lead_bbox "
-                command += " --store_config " + str(1)
-                command += " --lead_name_bbox "
-                command += " --random_dc " + str(next(iter_P3)) + " --random_bw " + str(next(iter_P19)) 
-                command += " --num_columns " + str(next(iter_P20)) 
-                command += " --full_mode " 
-                for lead in currentLeads:
-                    command += lead + " "
-                if hw_text:
-                    command += " --hw_text "
-                    command += " --deterministic_num_words --num_words " + str(num_words)
-                    command += " --deterministic_offset --x_offset " + str(x_offset) + " --y_offset " + str(y_offset)
-                    command += " --link " + str(args.link) + " --handwriting_size_factor " + str(next(iter_P9))
-                if wrinkles:
-                    command += " --wrinkles "
-                    command += " --deterministic_angle --crease_angle " + str(crease_angle)
-                    command += " --deterministic_vertical --num_creases_vertically " + str(num_creases_vertically)
-                    command += " --deterministic_horizontal --num_creases_horizontally " + str(num_creases_horizontally)
-                if augment:
-                    command += " --augment "               
-                    command += " --deterministic_noise --noise " + str(noise) 
-                    command += " --crop " + str(crop) + " --rotate " + str(next(iter_P13))
-
-                 
-                command += '\n'
-                f.write(command) 
+        with open(os.path.join(output_dir, 'textfile.pkl'), 'wb') as f:  
+            pickle.dump(all_commands, f)
                 #subprocess.call(command, shell=True)   
                   
 
