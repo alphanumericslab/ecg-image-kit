0cffaabd46fd9743f62ee4a8d78e8cdc2264d767
Deepanshi <deepanshi.asr.21@gmail.com>
2024-07-11 13:38:05 -0400

    ecg gen: generate images from pickle file

--- codes/ecg-image-generator/batch_run_script.py
+++ codes/ecg-image-generator/batch_run_script.py
@@ -10,30 +10,44 @@ import time
 
 f1 = os.path.join(os.getcwd(), "../../../output/new1/textfile.pkl")
 
-with open(f1, 'rb') as f:
-    loaded_list = pickle.load(f)
 
-st_time = time.time()
-for line in loaded_list[:10]:
-    print(line)
-    process = subprocess.Popen(line, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+def run_subprocess(cmd):
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     stdout, stderr = process.communicate()
-    
+        
     if process.returncode == 0:
         print("Script executed successfully")
         print("Output:", stdout)
     else:
         print("Script execution failed")
         print("Error:", stderr)
+        
 
-en_time = time.time()
-print(en_time - st_time)
+def run():
+    with open(f1, 'rb') as f:
+        loaded_list = pickle.load(f)
 
-def run_subprocess(cmd):
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    st_time = time.time()
+    '''for line in loaded_list[:10]:
+        print(line)
+        process = subprocess.Popen(line, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        stdout, stderr = process.communicate()
+        
+        if process.returncode == 0:
+            print("Script executed successfully")
+            print("Output:", stdout)
+        else:
+            print("Script execution failed")
+            print("Error:", stderr)
 
-st_time = time.time()
-with multiprocessing.Pool() as pool:
-        pool.map(run_subprocess, loaded_list[:10])
+    en_time = time.time()
+    print(en_time - st_time)'''
 
-print(time.time() - st_time)
+    st_time = time.time()
+    with multiprocessing.Pool() as pool:
+            pool.map(run_subprocess, loaded_list[:10])
+
+    print(time.time() - st_time)
+
+if __name__ == '__main__':
+    run()
