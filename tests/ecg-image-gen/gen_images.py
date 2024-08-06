4b9356fc00d619f020bc6090d36ce085e802ca3a
Deepanshi <deepanshi.asr.21@gmail.com>
2024-08-06 14:20:44 -0400

    pip package: test script

---
+++ tests/ecg-image-gen/gen_images.py
@@ -0,0 +1,8 @@
+from ecg_image_generator import gen_batch_images, gen_image
+import os, sys, argparse
+
+
+
+#generate simple file    
+gen_batch_images(input_dir='', output_dir='')
+gen_image(input_file='', header_file='', output_dir='')
